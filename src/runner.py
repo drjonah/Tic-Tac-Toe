84ea57cefc20fc7d707a49932ec13283e8a3356e
Jonah <jonahboy03@gmail.com>
2022-04-08 00:03:34 -0700

    runner changes

--- src/runner.py
+++ src/runner.py
@@ -1,5 +1,3 @@
-from cli import run_cli
-from gui import run_gui
 import json
 
 
@@ -7,15 +5,17 @@ def main():
     # load json
     with open('./src/settings.json', 'r') as FILE:
         settings = json.load(FILE)
-    
+
     # parse jsoon
     gamemode = settings['mode']
     difficulty = settings['ai difficulty']
 
     # run game
     if gamemode == 'cli':
+        from cli import run_cli
         run_cli(difficulty)
     elif gamemode == 'gui':
+        from gui import run_gui
         run_gui(difficulty)
     else:
         print('Unknown gamemode!')
